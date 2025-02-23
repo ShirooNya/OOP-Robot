5b68f2c4e542573deec7a28e89ab4e97808a48a4
ShirooNya <artemmarch223311@gmail.com>
2025-02-23 14:04:42 +0500

    Исключил возможность утечки ресурсов и добавил очередь сообщений

--- robots/src/log/LogWindowSource.java
+++ robots/src/log/LogWindowSource.java
@@ -12,68 +12,62 @@ import java.util.Collections;
  * величиной m_iQueueLength (т.е. реально нужна очередь сообщений 
  * ограниченного размера) 
  */
-public class LogWindowSource
-{
+
+public class LogWindowSource {
     private int m_iQueueLength;
-    
-    private ArrayList<LogEntry> m_messages;
+    private final ArrayList<LogEntry> m_messages;
     private final ArrayList<LogChangeListener> m_listeners;
     private volatile LogChangeListener[] m_activeListeners;
-    
-    public LogWindowSource(int iQueueLength) 
-    {
+
+    public LogWindowSource(int iQueueLength) {
         m_iQueueLength = iQueueLength;
-        m_messages = new ArrayList<LogEntry>(iQueueLength);
-        m_listeners = new ArrayList<LogChangeListener>();
+        m_messages = new ArrayList<>(iQueueLength);
+        m_listeners = new ArrayList<>();
     }
-    
-    public void registerListener(LogChangeListener listener)
-    {
-        synchronized(m_listeners)
-        {
+
+    public void registerListener(LogChangeListener listener) {
+        synchronized (m_listeners) {
             m_listeners.add(listener);
             m_activeListeners = null;
         }
     }
-    
-    public void unregisterListener(LogChangeListener listener)
-    {
-        synchronized(m_listeners)
-        {
+
+    public void unregisterListener(LogChangeListener listener) {
+        synchronized (m_listeners) {
             m_listeners.remove(listener);
             m_activeListeners = null;
         }
     }
-    
-    public void append(LogLevel logLevel, String strMessage)
-    {
+
+    public void append(LogLevel logLevel, String strMessage) {
+
         LogEntry entry = new LogEntry(logLevel, strMessage);
         m_messages.add(entry);
-        LogChangeListener [] activeListeners = m_activeListeners;
-        if (activeListeners == null)
-        {
-            synchronized (m_listeners)
-            {
-                if (m_activeListeners == null)
-                {
-                    activeListeners = m_listeners.toArray(new LogChangeListener [0]);
+
+        if (m_messages.size() > m_iQueueLength) {
+            m_messages.removeFirst();
+        }
+
+
+        LogChangeListener[] activeListeners = m_activeListeners;
+        if (activeListeners == null) {
+            synchronized (m_listeners) {
+                if (m_activeListeners == null) {
+                    activeListeners = m_listeners.toArray(new LogChangeListener[0]);
                     m_activeListeners = activeListeners;
                 }
             }
         }
-        for (LogChangeListener listener : activeListeners)
-        {
+        for (LogChangeListener listener : activeListeners) {
             listener.onLogChanged();
         }
     }
-    
-    public int size()
-    {
+
+    public int size() {
         return m_messages.size();
     }
 
-    public Iterable<LogEntry> range(int startFrom, int count)
-    {
+    public Iterable<LogEntry> range(int startFrom, int count) {
         if (startFrom < 0 || startFrom >= m_messages.size())
         {
             return Collections.emptyList();
